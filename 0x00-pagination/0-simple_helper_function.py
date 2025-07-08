37a81026131abe09c13814ce43bad9835a2fdea5
Ken Wekesa <wanyamak884@gmail.com>
2025-07-08 23:38:57 +0300

    simple pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,10 +2,8 @@
 """Simple helper function"""
 
 
-def index_range(page, page_size):
-    """return a tuple of size two containing a start index and an end index
-        corresponding to the range of indexes to return in a list for those
-        particular pagination parameters.
+def index_range(page: int, page_size: int) -> tuple:
+    """return a tuple of size two with start index and end index
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,40 @@
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+    
+    def index_range(self, page: int, page_size: int) -> tuple:
+        """return a tuple of size two with start index and end index
+        """
+        start_index = (page - 1) * page_size
+        end_index = start_index + page_size
+
+        return (start_index, end_index)
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert type(page) == int and type(page_size) == int
+            assert page > 0 and page_size > 0 and type(page)
+            idx_range = self.index_range(page, page_size)
+            dataset = self.dataset()
+            if idx_range:
+                return dataset[idx_range[0]:idx_range[1]]
+            return []
