8b8d969e5ad97e8e7acd3ec8e1314f999dee9035
Ken Wekesa <wanyamak884@gmail.com>
2025-07-11 06:21:17 +0300

    Issue was that the cache base class did not employ OrderedDict class

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,6 +2,7 @@
 """ FIFO caching
 """
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class FIFOCache(BaseCaching):
@@ -16,17 +17,17 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add item to cache at given key
         """
+        self.cache_data[key] = item
+        keys_list = list(self.cache_data)
         if key is None or item is None:
             return None
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            key_list = list(self.cache_data)
-            print(f"DISCARD: {key_list[0]}")
-            self.cache_data.pop(0)
-        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            self.cache_data.pop(keys_list[0])
+            print(f"DISCARD: {keys_list[0]}")
 
     def get(self, key):
         """ Get item given key
         """
-        if key is not None and key in self.cache_data:
-            return self.cache_data
+        if key and key in self.cache_data:
+            return self.cache_data[key]
         return None
