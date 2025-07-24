4c12bceaeb00bfa90b687ae10b4e680714f58de9
Ken Wekesa <wanyamak884@gmail.com>
2025-07-24 10:37:33 +0300

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask_babel import Babel
+app = __import__('0-app.py').app
+
+
+class Config:
+    """configure available languages, locale and timezone in app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
