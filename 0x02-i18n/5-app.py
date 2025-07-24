ba3abac2fc1fa0738d276eaa459b9c995eb9cb54
Ken Wekesa <wanyamak884@gmail.com>
2025-07-24 15:57:15 +0300

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Parametrize templates"""
+import flask
 from flask import (
     Flask,
     render_template,
@@ -10,6 +11,13 @@ from flask_babel import (
     _,
     gettext
 )
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 app = Flask(__name__)
 
@@ -25,6 +33,22 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+def get_user():
+    """returns a user dictionary or None if the ID cannot be found or 
+        if login_as was not passed.
+    """
+    _id = request.args.get('login_as')
+    if not _id or int(_id) not in users.keys():
+        return None
+    return users[int(_id)]
+
+
+@app.before_request
+def before_request():
+    """find a user if any and set it as a global on flask.g.user"""
+    flask.g.user = get_user()
+
+
 @babel.localeselector
 def get_locale():
     """determine best match with supported languages"""
@@ -37,7 +61,7 @@ def get_locale():
 @app.route('/')
 def index():
     """default route"""
-    return render_template('4-index.html')
+    return render_template('5-index.html', get_user=get_user)
 
 
 if __name__ == "__main__":
