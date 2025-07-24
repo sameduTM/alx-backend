4375646f8dec150d3d51f676a91acfa12a8d2eb3
Ken Wekesa <wanyamak884@gmail.com>
2025-07-24 10:29:12 +0300

    Basic Babel setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,8 +2,19 @@
 """Basic Flask app"""
 from flask import Flask
 from flask import render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Class to configure available languages in app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
