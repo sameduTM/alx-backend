db4c19a964e2f85f554505662ee31dd1f587c414
Ken Wekesa <wanyamak884@gmail.com>
2025-07-24 13:52:00 +0300

    parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,3 +1,43 @@
 #!/usr/bin/env python3
 """Parametrize templates"""
-from flask_babel import gettext
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import (
+    Babel,
+    _,
+    gettext
+)
+
+app = Flask(__name__)
+
+
+class Config:
+    """configures available languages in app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine best match with supported languages"""
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/')
+def index():
+    """default route"""
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
