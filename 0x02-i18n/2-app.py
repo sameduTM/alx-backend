cc005083e55e6891701bb335a5438746a99b24f9
Ken Wekesa <wanyamak884@gmail.com>
2025-07-24 10:57:28 +0300

    remove error prone codde

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Get locale from request"""
-from flask import g, request
+from flask import request
 
 babel = __import__('1-app').babel
-config = __import__('1-app').Config
+Config = __import__('1-app').Config
 
 
 @babel.localeselector
 def get_locale():
     """method to determine the best match with our supported languages"""
-    return request.accept_languages.best_match(config.LANGUAGES)
+    return request.accept_languages.best_match(Config.LANGUAGES)
